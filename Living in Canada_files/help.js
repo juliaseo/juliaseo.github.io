15ed0de402552e58d79923a750beef5f229fa6f4
juliaseo <73679215+juliaseo@users.noreply.github.com>
2021-03-20 13:20:11 -0400

    Add files via upload

---
+++ Living in Canada_files/help.js
@@ -0,0 +1,17 @@
+﻿function toggleOn(divId) {
+    var el = document.getElementById(divId);
+    el.style.display = 'block';
+}
+function toggleOff(divId) {
+    var el = document.getElementById(divId);
+    el.style.display = 'none';
+}
+function toggle(divId) {
+    var el = document.getElementById(divId);
+    if (el.style.display == 'none') { el.style.display = 'block'; }
+    else { el.style.display = 'none'; }
+}
+
+
+
+
